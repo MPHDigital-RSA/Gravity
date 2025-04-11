cb697a48531fdfe644e1e14e05e23fbd6c15a711
Prince Hlela <mphdigital@protonmail.com>
2025-04-11 12:04:04 +0200

    created the services section

---
+++ src/data/portfolioItems.js
@@ -0,0 +1,36 @@
+import imageOne from '../assets/heroImage.png'
+import imageTwo from '../assets/messageImage.png'
+import imageThree from '../assets/servicesImage.png'
+
+ const portfolioItems = [
+    {
+        image: imageOne,
+        category: 'web dev',
+        title: 'Web Development',
+        titleColor: '#1434E1',
+        titleBg: "#FFF"
+    },
+    {
+        image: imageTwo,
+        category: 'marketing',
+        title: 'Email Marketing',
+        titleColor: 'white',
+        titleBg: "#ef8e70e0"
+    },
+    {
+        image: imageThree,
+        category: 'UI design',
+        title: 'UI/UX Design',
+        titleColor: '#1434E1',
+        titleBg: "#ffffff"
+    },
+    {
+        image: imageOne,
+        category: 'seo',
+        title: 'SEO',
+        titleColor: 'white',
+        titleBg: "#ef8e70"
+    }
+]
+
+export default portfolioItems
--- src/section-components/Portfolio.jsx
+++ src/section-components/Portfolio.jsx
@@ -1,16 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/Portfolio.css'
+import ButtonSolid from '../components/ButtonSolid'
 
-const filterLinks = [
-    'all',
-    'branding',
-    'web dev',
-    'marketing'
-]
+import portfolioItems from '../data/portfolioItems'
 
 function Portfolio() {
+
+    const [items, setItems] = useState(portfolioItems);
+
+    // create a new array with filtered items according to a category value
+    const filterLinks = [... new Set(portfolioItems.map((value) => value.category))];
+
+    const filterItems = (cat) => {
+        const newData = portfolioItems.filter((value) => value.category === cat);
+        setItems(newData);
+    }
+
     return (
         <div className='wrapper portfolio'>
+
             <div className="subtitle">
                 <div className="line"></div>
                 <span>Portfolio</span>
@@ -20,12 +28,32 @@ function Portfolio() {
                 <h2>Discover our Work</h2>
 
                 <div className="filter">
+                    <button onClick={() => setItems(portfolioItems)}> All</button>
                     {filterLinks.map((link, index) => (
-                        <button key={index}>
+                        <button key={index} onClick={() => filterItems(link)}>
                             {link}
                         </button>
                     ))}
                 </div>
+            </div>
+
+            <div className="portfolio-grid">
+                {items.map((item, index) => (
+                    <div className='portfolio-item' key={index} style={{ backgroundImage: `url(${item.image})` }}>
+                        <span style={{
+                            backgroundImage: `url(${item.image})`,
+                            color: `${item.titleColor}`,
+                            background: `${item.titleBg}`
+
+                        }}>
+                            {item.title}
+                        </span>
+                    </div>
+                ))}
+            </div>
+
+            <div className="button">
+                <ButtonSolid text='+ projects' link='#' />
             </div>
 
         </div>
