6f9758b49672abb2d2c85e4e2707fb6a6e79e154
Prince Hlela <mphdigital@protonmail.com>
2025-04-14 17:27:57 +0200

    create our clients section

--- src/section-components/OurClients.jsx
+++ src/section-components/OurClients.jsx
@@ -10,17 +10,15 @@ import apple from '../assets/apple.svg'
 function OurClients() {
     return (
         <div className='wrapper our-clients'>
-            <div className='our-clients__text'>
-                <h2>Our Clients</h2>
-                <div className="our-clients__description-subtitle">
-                    <div className="subtitle">
-                        <div className="line"></div>
-                        <span>We worked with</span>
-                    </div>
-
-                    <p className='our-clients__description'>Our favorite brands are our friends! We help them to achieve their goals, they help us to stay sharp.</p>
-                </div>
+            <h2 className='our-clients__heading'>Our Beloved Clients</h2>
+            <div className="subtitle our-clients__subtitle">
+                <div className="line"></div>
+                <span>We worked with</span>
             </div>
+
+            <p className='our-clients__description'>Our favorite brands are our friends! We help them to achieve their goals, they help us to stay sharp.</p>
+
+
             <div className='our-clients__images'>
                 <img src={xiaomi} alt="xiaomi logo" width="80" height="62.86" />
                 <img src={tinder} alt="xiaomi logo" width="52" height="69.03" />
